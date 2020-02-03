282863e3f417c9d3174841a6a39581e8d2b4eff8
KanwalSaeed <kanwal_saeed122@hotmail.com>
2020-02-03 21:46:40 +0500

    first code

---
+++ 5. ESCAPE SEQUENCE IN PYTHON.py
@@ -0,0 +1,22 @@
+#ESCAPE SEQUENCE INN PYTHON
+
+message = "This is your's labtop"
+print(message)
+
+tweet = 'This is "PRO" man'
+print(tweet)
+
+
+msg = 'This is you\'r laptop'
+print(msg)
+tweet = "This is \"PRO\" man"
+print(tweet)
+
+#\n
+print("I am John\nI am 25\nI am a student of AI")
+
+
+#\t add six spaces
+print("Hello,\t This is Kanwal.")
+
+
